4bf4f676d9cdf68f5d74203dd52181eb9d5ec7b9
sdutta-ux <sdutta@vixenindia.com>
2026-02-13 16:43:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-// Google Apps Script Web App URL
+// Your Google Apps Script Web App URL
 const EXEC_URL = "https://script.google.com/macros/s/AKfycbxY39zbyEm3Ov6gNdrsaV7QHiY0zS-rRcmydYE5NdjP2-TUHvCynat4NjSNL5WpXfyQow/exec";
