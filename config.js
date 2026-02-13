66c78740ee4341e10d09bdcab1fe207441c2278e
sdutta-ux <sdutta@vixenindia.com>
2026-02-13 16:39:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,2 @@
-// config.js - update EXEC_URL to your Apps Script exec URL
-const CONFIG = {
-  EXEC_URL: "https://script.google.com/macros/s/AKfycbw7PwHzuAtWmhAKSA9oawLfWpAVdVEh_pEikO-RlSYNz4DlpRboxEJRul95HIrJLmeESQ/exec"
-};
+// Google Apps Script Web App URL
+const EXEC_URL = "https://script.google.com/macros/s/AKfycbxY39zbyEm3Ov6gNdrsaV7QHiY0zS-rRcmydYE5NdjP2-TUHvCynat4NjSNL5WpXfyQow/exec";
